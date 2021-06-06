079c7ad7d85878b6f73af480d32143ae69b0ba82
Max Volkov <maxim.volkov.ev@gmail.com>
2021-06-07 00:20:21 +1200

    backup

--- connector.framework.cmd/Program.cs
+++ connector.framework.cmd/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,48 @@ namespace connector.framework.cmd
 {
   class Program
   {
+    private static CommandArgument importCmdArgument, exportCmdArgument;
+    private static CommandLineApplication commandLineApplication;
+    private static CommandOption managementServer;
     static void Main(string[] args)
     {
+      commandLineApplication = new CommandLineApplication(throwOnUnexpectedArg: true);
+      commandLineApplication.Command("import", ConfigureImport, throwOnUnexpectedArg: true).OnExecute(DoImport);
+      commandLineApplication.Command("export", ConfigureExport, throwOnUnexpectedArg: true).OnExecute(DoExport);
+      managementServer = commandLineApplication.Option("-m |--managementServer <server_fqdn>", "SCOM Management Server address", CommandOptionType.SingleValue);
+      commandLineApplication.HelpOption("-? | -h | --help");
+      commandLineApplication.VersionOption("-v", "1.0");
+
+      commandLineApplication.OnExecute(DefaultMain);
+      commandLineApplication.Execute(args);
+    }
+
+    private static int DoExport()
+    {
+      Console.WriteLine($"DoExport: {exportCmdArgument.Value}, {managementServer.Value()}");
+      return 0;
+    }
+
+    private static int DoImport()
+    {
+      Console.WriteLine($"DoImport: {importCmdArgument.Value}");
+      return 0;
+    }
+
+    private static void ConfigureImport(CommandLineApplication obj)
+    {
+      importCmdArgument = obj.Argument("filename.json", "Path to a JSON file with object instance information.", multipleValues: false);
+    }
+
+    private static void ConfigureExport(CommandLineApplication obj)
+    {
+      exportCmdArgument = obj.Argument("filename.json", "Path to a JSON file where to save object instance information.", multipleValues: false);
+    }
+
+    private static int DefaultMain()
+    {
+      Console.WriteLine("Exec");
+      return 0;
     }
   }
 }
